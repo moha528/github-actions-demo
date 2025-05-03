b1c7f17b48e416e925788f868041ffd3e108e0f3
Mouhamadou Tidiane Seck <mtseckdev@gmail.com>
2025-05-03 04:14:35 +0000

    fix

--- index.test.js
+++ index.test.js
@@ -8,10 +8,7 @@ test('adds 4 + 5 to equal 9', () => {
   expect(sum(4, 5)).toBe(9);
 });
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('adds 10 + 2 to equal 12', () => {
+  expect(sum(10, 2)).toBe(12);
 });
 
-test('adds 10 + 2 to equal 15', () => {
-  expect(sum(10, 2)).toBe(15);
-});
