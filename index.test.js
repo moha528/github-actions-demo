7fb2915c0dc410a15313dddd495f66293391ed84
Mouhamadou Tidiane Seck <mtseckdev@gmail.com>
2025-05-03 04:11:00 +0000

    erreur volontaire

--- index.test.js
+++ index.test.js
@@ -2,4 +2,16 @@ const sum = require('./index');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
-}); 
+});
+
+test('adds 4 + 5 to equal 9', () => {
+  expect(sum(4, 5)).toBe(9);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 10 + 2 to equal 15', () => {
+  expect(sum(10, 2)).toBe(15);
+});
