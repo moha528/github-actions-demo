d73daf6825fe592e33c9359a0f174f7fec7d75a5
Mouhamadou Tidiane Seck <mtseckdev@gmail.com>
2025-05-24 08:40:05 +0000

    fix blocking unit tests

--- index.js
+++ index.js
@@ -15,8 +15,11 @@ app.post('/todos', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 module.exports = app; // Export pour les tests 
